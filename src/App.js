ca4163abc2b6556c71f3d7e4210fc93e5bce0f57
mwh1717 <michael.wells.howell@gmail.com>
2022-02-27 12:37:59 -0500

    backspace deleting from currentGuess

--- src/App.js
+++ src/App.js
@@ -9,22 +9,30 @@ function App() {
   // handler for key presses
   const handleKeyUp = (event) => {
     onLetterGuess(event.key);
-    console.log('key pressed = ', event.key)
+    onBackspace(event.key);
   }
 
-  // sets state on letter guess
+  // sets stat on backspace, slices currentGuess
+  const onBackspace = (press) => {
+    if (press === 'Backspace') {
+      setCurrentGuess(currentGuess.slice(0, -1));
+    }
+  }
+
+  // sets state on letter guess, including currentGuess
   const onLetterGuess = (guess) => {
-    setCurrentGuess(guess);
+    if (currentGuess.length <= 4) {
+      setCurrentGuess(`${currentGuess}${guess}`);
+    }
   }
 
-  // adds key listener one time on mount
   useEffect(() => {
     document.addEventListener('keyup', handleKeyUp);
   
     return () => {
       document.removeEventListener('keyup', handleKeyUp);
     }
-  }, [])
+  }, [onLetterGuess, onBackspace])
 
   return (
     <div className="App" onKeyUp={handleKeyUp}>
